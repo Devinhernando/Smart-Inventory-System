b5b7db2baa90aab79da16078dee9c1c88bc9305d
devinhernando <dvnhernandohuang@gmail.com>
2026-04-18 14:23:01 +0700

    update app

--- app.py
+++ app.py
@@ -58,3 +58,6 @@ def delete(id):
         return jsonify({"error": err}), 404
 
     return jsonify({"message": "Deleted"}), 200
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)
